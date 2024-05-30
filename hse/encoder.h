dedbd800128a912a275158b021f33d0cdbb369fd
Ned Bingham <edward.bingham@broccolimicro.io>
2024-05-30 09:56:45 -0400

    documenting the encoder

--- hse/encoder.h
+++ hse/encoder.h
@@ -16,31 +16,83 @@ namespace hse
 
 struct graph;
 
+// Our goal is to convert the HSE graph into production rules. In doing so, we
+// lose the syntactic constraints on event orderings and replace them
+// exclusively with conditions on the current state encoding. In order to do
+// this, we need to make sure that there is enough information in those state
+// encodings to disambiguate states in which a transition should fire from
+// states in which a transition should not. When there is not enough
+// information to do so, then that is a state conflict. More specifically, a
+// conflict is a contiguous region of places in which a transition can fire
+// when it should not. Our goal is to eliminate these conflicts by creating a
+// state variable in which the variable's value is different between those two
+// regions. Transitions on this new variable create a graph cut that separates
+// the states in which the transition should fire from the states in which it
+// should not.
 struct conflict
 {
 	conflict();
 	conflict(term_index index, int sense, vector<petri::iterator> implicant, vector<petri::iterator> region);
 	~conflict();
 
-	// index of the cube in the transition and the transition in graph
+	// index of the transition in the graph and the cube in the transition
 	term_index index;
 
-	// up conflict or down conflict or non-directional
+	// CMOS logic is inverted. PMOS transistors pull the output high when the
+	// gate is low and NMOS transistors pull the output low when the gate is
+	// high. Therefore, upgoing transitions must use inverted literals (~x -> y+)
+	// and downgoing transitions must use non-inverted literals (x -> y-).
+
+	// There are three types of conflicts depending on how we're approaching the problem:
+	
+	enum {
+		// a non-directional conflict ignores the fact that CMOS logic is inverting
+		// and uses all of the state information in the predicates of the two
+		// places to determine whether they are sufficiently disambiguated.
+		NONE = -1,
+		// a downgoing conflict ignores all inverted literals in the predicates of
+		// the two places.
+		DOWN = 0,
+		// an upgoing conflict ignores all non-inverted literals in the predicates
+		// of the two places.
+		UP = 1
+	};
 	int sense;
 
-	// input places leading to transition
+	// the input places leading to the transition
 	vector<petri::iterator> implicant;
 
-	// region of nodes the transition can fire in incorrectly
+	// region of places the transition can fire in incorrectly
 	vector<petri::iterator> region;
 };
 
+// Unfortunately, it is not enough to put new transitions in to disambiguate
+// states. Inserting those transitions can create new conflicts with those
+// transitions. The input places of the inserted transition might conflict with
+// some other region where the state variable needs to have the opposite value.
+// We can get some understanding of this by comparing the predicates for
+// various pairs of places. If they are not disambiguated, then they are
+// considered a state suspect. It means that if we insert a transition that
+// uses one of these places as an input place, then that transition will also
+// fire in the other place and that might create a conflict.
 struct suspect
 {
 	suspect();
 	suspect(int sense, vector<petri::iterator> first, vector<petri::iterator> second);
 	~suspect();
 
+	enum {
+		// a non-directional conflict ignores the fact that CMOS logic is inverting
+		// and uses all of the state information in the predicates of the two
+		// places to determine whether they are sufficiently disambiguated.
+		NONE = -1,
+		// a downgoing conflict ignores all inverted literals in the predicates of
+		// the two places.
+		DOWN = 0,
+		// an upgoing conflict ignores all non-inverted literals in the predicates
+		// of the two places.
+		UP = 1
+	};
 	int sense;
 	vector<petri::iterator> first;
 	vector<petri::iterator> second;
