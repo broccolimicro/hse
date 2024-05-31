46621bdafb3a500531d4b15232e7df97050a3837
Ned Bingham <edward.bingham@broccolimicro.io>
2024-05-31 19:30:20 -0400

    more documentation

--- hse/synthesize.h
+++ hse/synthesize.h
@@ -14,14 +14,33 @@
 namespace hse
 {
 
+// A gate is a pull up and pull down network that drives a specific
+// variable/circuit node. The variable in question is specified by this gate's
+// location in the array of gates for this circuit, and is not recorded in the
+// gate struct specifically. Each gate is synthesized from the fully elaborated
+// HSE using the place predicates.
 struct gate
 {
+	// The value of this variable on reset.
 	int reset;
+
+	// Each of these are an array of size 2. Index 0 is the pull down
+	// network and index 1 is the pull up network.
+
+	// The list of all transitions that drive the node low or high.
 	vector<petri::iterator> tids[2];
+
+	// The set of states in which the pull down or pull up network
+	// must be active and driving the variable through a transition.
 	boolean::cover implicant[2];
+
+	// The set of states in which the pull down or pull up network
+	// must be inactive.
 	boolean::cover exclusion[2];
+	
+	// The set of states in which the variable needs to hold a low or
+	// a high value.
 	boolean::cover holding[2];
-	boolean::cover keeper[2];
 
 	bool is_combinational();
 	
